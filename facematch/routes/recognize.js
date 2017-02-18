6fe030c56951a7e68f2d297f3925912c2e4c8ad6
Scott Miller <sam6258@psu.edu>
2017-02-18 13:46:21 -0500

    Add facematch minus config.js for security

---
+++ facematch/routes/recognize.js
@@ -0,0 +1,40 @@
+var recognize = require('../lib/recognize'),
+    graph = require('fbgraph');
+
+/*!
+ * Calls the recognize function
+ */
+
+module.exports = function(req, res) {
+
+  // vars
+  var imgUrl = req.body.url,
+      accessToken = req.body.accessToken;
+
+  // set access_token to upload image
+  graph.setAccessToken(accessToken);
+
+  // upload image
+  var params = {
+    url: imgUrl, 
+    message:'temp', 
+    privacy: { value: 'SELF' } // we don't want other people to see it
+  };
+
+  graph.post('/me/photos', params, function(err, r) {
+    
+    // we have the imgId! now we can ask Facebook to recognize my friends
+    var imgId = r.id;
+
+    // wait 3 seconds before asking Facebook (they recognize asynchronously)
+    setTimeout(function() {
+      recognize(imgId, function(result) {
+        if(result.length === 0) {
+          res.send({ error: 'Facebook couldn\'t recognize this picture.' });
+        } else {
+          res.send(result);
+        }
+      })
+    }, 3000)
+  });
+};
